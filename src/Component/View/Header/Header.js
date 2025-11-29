b26e74fd2eb02a8cf879ead0b7698d4ae12d7eff
khanh699 <khanh.duong.a2h@gmail.com>
2025-11-29 13:12:21 +0700

    khanh

--- src/Component/View/Header/Header.js
+++ src/Component/View/Header/Header.js
@@ -1,68 +1,6 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import Button from 'react-bootstrap/Button';
-// import Container from 'react-bootstrap/Container';
-// import Form from 'react-bootstrap/Form';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-// import { NavLink, useNavigate } from "react-router-dom";
-
-// const Header = () => {
-//   // const navigate = useNavigate();
-  
-//   // const HandleLogin = () => {
-//   //   navigate('/login');
-//   // }
-//   // const HandleRegister = () => {
-//   //   navigate('/register');
-//   // }
-
-//   return (
-//       <Navbar expand="lg" className="bg-body-tertiary">
-//           <Container fluid>
-//          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-//          <Navbar.Toggle aria-controls="navbarScroll" />
-//          <Navbar.Collapse id="navbarScroll">
-//            <Nav
-//              className="mx-auto my-2 my-lg-0"
-//              navbarScroll
-//            >
-//              <Nav.Link href="#action1">Home</Nav.Link>
-//              <Nav.Link href="#action2">Link</Nav.Link>
-//              <NavDropdown title="Link" id="navbarScrollingDropdown">
-//                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-//                <NavDropdown.Item href="#action4">
-//                  Another action
-//                </NavDropdown.Item>
-//                <NavDropdown.Divider />
-//                <NavDropdown.Item href="#action5">
-//                  Something else here
-//                </NavDropdown.Item>
-//              </NavDropdown>
-//              <Nav.Link href="#" disabled>
-//                Link
-//              </Nav.Link>
-//            </Nav>
-//            <Form className="d-flex">
-//              <Form.Control
-//                type="search"
-//                placeholder="Search"
-//                className="me-2"
-//                aria-label="Search"
-//              />
-//              <Button variant="outline-success">Search</Button>
-//            </Form>
-//          </Navbar.Collapse>
-//        </Container>
-//      </Navbar>
-//   );
-// }
-
-// export default Header;
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { NavLink } from "react-router-dom";
-// import { useState } from 'react';
+import { useState } from 'react';
 import { GrSearch } from "react-icons/gr";
 import { BsBag } from "react-icons/bs";
 import { FaGripLines } from "react-icons/fa6";
@@ -70,79 +8,61 @@ import { IoLogoApple, IoMdClose } from "react-icons/io";
 import { NavLink, useNavigate } from "react-router-dom";
 
 const Header = (props) => {
-  // const [isShowNav, setIsShowNav] = useState(true);
-
-  // const Responsive = () => {
-  //   // const respon = document.querySelector("div.respon");
-  //   // console.log(respon);
-    
-  //   // respon.style.display = "flex";
-  //   // alert("my");
-  //   setIsShowNav(!isShowNav);
-  // }
-
-  // const navigate = useNavigate();
-
-  // const HandleLogin = () => {
-  //   navigate('/login');
-  // }
-  // const HandleRegister = () => {
-  //   navigate('/register');
-  // }
+  const [isShowNav, setIsShowNav] = useState(false);
+  
+  const HandleNav = () => {
+    setIsShowNav(!isShowNav);
+  }
 
   return(
     <>
       <div className="header-container">
         <div className='header-background'>
           <div className="logo"><NavLink to="/" className="navbar-brand"><IoLogoApple /></NavLink></div>
-          {/* <label className="responsive" htmlFor='close'>&#9776;</label>
-          <input className='input' hidden type='checkbox' id='close'/> */}
           <div className="responsive">
-          <label htmlFor='close'><FaGripLines /></label>
-          <input className='input' hidden type='checkbox' id='close'/>
-          {/* <button onClick={() => Responsive()}>&#9776;</button>
-          <button type="button" className="btn-close" aria-label="Close"></button> */}
-          <label htmlFor='close' className='background'></label>
-          <div className="list">
-            <label htmlFor='close' className="close" aria-label="Close"><IoMdClose /></label>
-            <div className="box">
-              <h3><NavLink to="/store" className="navbar-brand">Store</NavLink></h3>
-              <div className=""></div>
+            <label htmlFor='close' onClick={() => {HandleNav()}}><FaGripLines /></label>
+            <input className='input' hidden type='checkbox' id='close'/>
+            <label htmlFor='close' className='background' style={isShowNav ? {display : "block"} : {display : "none"}}></label>
+            <div className="list" style={isShowNav ? {transform : "translateX(0)"} : {transform : "translateX(100%)"}}>
+              <label htmlFor='close' className="close" aria-label="Close" onClick={() => {HandleNav()}}><IoMdClose /></label>
+              <div className="box">
+                <h3 onClick={() => {HandleNav()}}><NavLink to="/store" className="navbar-brand">Store</NavLink></h3>
+                <div className=""></div>
+              </div>
+              <div className="box">
+                <h3 onClick={() => {HandleNav()}}><NavLink to="/" className="navbar-brand">Mac</NavLink></h3>
+                <div></div>
+              </div>
+              <div className="box">
+                <h3 onClick={() => {HandleNav()}}><NavLink to="/" className="navbar-brand">iPad</NavLink></h3>
+                <div></div>
+              </div>
+              <div className="box">
+                <h3 onClick={() => {HandleNav()}}><NavLink to="/" className="navbar-brand">iPhone</NavLink></h3>
+                <div></div>
+              </div>
+              <div className="box">
+                <h3 onClick={() => {HandleNav()}}><NavLink to="/" className="navbar-brand">Watch</NavLink></h3>
+              </div>
+              <div className="box">
+                <h3 onClick={() => {HandleNav()}}><NavLink to="/" className="navbar-brand">Vision</NavLink></h3>
+              </div>
+              <div className="box">
+                <h3 onClick={() => {HandleNav()}}><NavLink to="/" className="navbar-brand">AirPods</NavLink></h3>
+              </div>
+              <div className="box">
+                <h3 onClick={() => {HandleNav()}}><NavLink to="/" className="navbar-brand">TV &#38; Home</NavLink></h3>
+              </div>
+              <div className="box">
+                <h3 onClick={() => {HandleNav()}}><NavLink to="/" className="navbar-brand">Entertainment</NavLink></h3>
+              </div>
+              <div className="box">
+                <h3 onClick={() => {HandleNav()}}><NavLink to="/" className="navbar-brand">Accessories</NavLink></h3>
+              </div>
+              <div className="box">
+                <h3 onClick={() => {HandleNav()}}><NavLink to="/" className="navbar-brand">Support</NavLink></h3>
+              </div>
             </div>
-            <div className="box">
-              <h3>Mac</h3>
-              <div></div>
-            </div>
-            <div className="box">
-              <h3>iPad</h3>
-              <div></div>
-            </div>
-            <div className="box">
-              <h3>iPhone</h3>
-              <div></div>
-            </div>
-            <div className="box">
-              <h3>Watch</h3>
-            </div>
-            <div className="box">
-              <h3>Vision</h3>
-            </div>
-            <div className="box">
-              <h3>AirPods</h3>
-            </div>
-            <div className="box">
-              <h3>TV &#38; Home</h3>
-            </div>
-            <div className="box">
-              <h3>Entertainment</h3>
-            </div>
-            <div className="box">
-              <h3>Accessories</h3>
-            </div>
-            <div className="box">
-              <h3>Support</h3>
-            </div>
-          </div>
           </div>
           <label htmlFor='close' className='background'></label>
           <div className="list">
@@ -151,37 +71,37 @@ const Header = (props) => {
               <div className=""></div>
             </div>
             <div className="box">
-              <h3>Mac</h3>
+              <h3><NavLink to="/" className="navbar-brand">Mac</NavLink></h3>
               <div></div>
             </div>
             <div className="box">
-              <h3>iPad</h3>
+              <h3><NavLink to="/" className="navbar-brand">iPad</NavLink></h3>
               <div></div>
             </div>
             <div className="box">
-              <h3>iPhone</h3>
+              <h3><NavLink to="/" className="navbar-brand">iPhone</NavLink></h3>
               <div></div>
             </div>
             <div className="box">
-              <h3>Watch</h3>
+              <h3><NavLink to="/" className="navbar-brand">Watch</NavLink></h3>
             </div>
             <div className="box">
-              <h3>Vision</h3>
+              <h3><NavLink to="/" className="navbar-brand">Vision</NavLink></h3>
             </div>
             <div className="box">
-              <h3>AirPods</h3>
+              <h3><NavLink to="/" className="navbar-brand">AirPods</NavLink></h3>
             </div>
             <div className="box">
-              <h3>TV &#38; Home</h3>
+              <h3><NavLink to="/" className="navbar-brand">TV &#38; Home</NavLink></h3>
             </div>
             <div className="box">
-              <h3>Entertainment</h3>
+              <h3><NavLink to="/" className="navbar-brand">Entertainment</NavLink></h3>
             </div>
             <div className="box">
-              <h3>Accessories</h3>
+              <h3><NavLink to="/" className="navbar-brand">Accessories</NavLink></h3>
             </div>
             <div className="box">
-              <h3>Support</h3>
+              <h3><NavLink to="/" className="navbar-brand">Support</NavLink></h3>
             </div>
           </div>
           <div className="user">
